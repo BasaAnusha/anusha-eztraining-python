cff9aef2f1c5575bf0b7787f77fc08c7cf81a32d
BasaAnusha <113752183+BasaAnusha@users.noreply.github.com>
2023-02-01 21:47:38 +0530

    Add files via upload

---
+++ pro2.py
@@ -0,0 +1,14 @@
+
+m=list(map,int(input("enter element:").split()))
+print(m)
+x=1
+y=0
+for i in m:
+    x=x*i
+    y-y+1
+if(x<=750):
+    
+       print("product",x)
+else:
+       print("sum",y)
+                                        
---
+++ set.py
@@ -0,0 +1,3 @@
+t=(1,3,5,7,9,"windows")
+print(t.count(5))
+print(t.index(9))
