8c2d697a5b568534d45072310775d7bb8f527041
BasaAnusha <113752183+BasaAnusha@users.noreply.github.com>
2023-02-03 20:48:12 +0530

    Add files via upload

--- program.py
+++ program.py
@@ -1,3 +1,13 @@
-print('its','a', 'good','day',end='')
-print("all" ,'is','good',sep="***",end='')
-print('joy')
+
+def lemon(t):
+  if t==24:
+         print("requirement satisfied")
+  else:
+         print("excess lemon:")
+         
+x=int(input("enter :"))
+y=int(input("enter :"))
+z=int(input("enter :"))
+t=x+y+z
+lemon(t)
+
